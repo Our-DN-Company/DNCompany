03958111792c449c17e4d98d9d40fd96b052d1fb
JiHwanOh-prog <dhwlghks159@naver.com>
2024-12-30 13:51:11 +0900

    settings

---
+++ src/main/java/com/example/dncompany/DNcompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.dncompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DNcompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DNcompanyApplication.class, args);
+    }
+
+}
