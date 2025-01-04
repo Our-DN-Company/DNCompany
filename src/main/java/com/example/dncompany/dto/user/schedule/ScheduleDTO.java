75f7f4ec7be68c22f1c43dd354e78d343ba46d08
JiHwanOh-prog <dhwlghks159@naver.com>
2025-01-04 23:07:18 +0900

    schedule Mapper, .xml 파파일 생성 및 수정

--- src/main/java/com/example/dncompany/dto/user/schedule/ScheduleDTO.java
+++ src/main/java/com/example/dncompany/dto/user/schedule/ScheduleDTO.java
@@ -4,7 +4,18 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 @Setter @Getter @ToString
 public class ScheduleDTO {
-
+    Long scheduleId;
+    String scheduleTitle;
+    String scheduleContent;
+    LocalDate scheduleDate;
+    LocalTime scheduleStartTime;
+    LocalTime scheduleEndTime;
+    Long usersId;
+    Long helpId;
 }
--- src/main/java/com/example/dncompany/mapper/user/schedule/ScheduleMapper.java
+++ src/main/java/com/example/dncompany/mapper/user/schedule/ScheduleMapper.java
@@ -1,8 +1,10 @@
 package com.example.dncompany.mapper.user.schedule;
 
+import com.example.dncompany.dto.user.schedule.ScheduleDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ScheduleMapper {
 
+    ScheduleDTO selectScheduleByUserId(Long usersId);
 }
