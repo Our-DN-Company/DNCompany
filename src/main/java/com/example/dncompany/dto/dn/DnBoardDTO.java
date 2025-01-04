02555206017db20c53546d91bd8be1431f745e20
he11omelon <whxorud96@gmail.com>
2025-01-05 00:59:32 +0900

    board ãhtmlã,ãcss 파일 추가 및 boardDTO 추가

---
+++ src/main/java/com/example/dncompany/dto/dn/DnBoardDTO.java
@@ -0,0 +1,16 @@
+package com.example.dncompany.dto.dn;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter @ToString
+public class DnBoardDTO {
+    Long dnId;
+    String dnCategory;
+    LocalDateTime dnCreatedAt;
+    LocalDateTime dnUpdatedAt;
+    Long userId;
+}
