fabd77279a73fc1c62432f189a7a929ef907849b
JiHwanOh-prog <dhwlghks159@naver.com>
2025-01-04 18:34:56 +0900

    add schedule mapper, DTO and modify schedule publish name

---
+++ src/main/java/com/example/dncompany/dto/user/schedule/ScheduleDTO.java
@@ -0,0 +1,10 @@
+package com.example.dncompany.dto.user.schedule;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter @Getter @ToString
+public class ScheduleDTO {
+
+}
