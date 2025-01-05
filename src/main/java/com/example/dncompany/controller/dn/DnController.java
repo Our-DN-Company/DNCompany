c3d1e9b1d1e85264191bcf86da13cbab5c7bf67c
JiHwanOh-prog <dhwlghks159@naver.com>
2025-01-05 10:26:24 +0900

    에러 사항 수정중

--- src/main/java/com/example/dncompany/controller/dn/DnController.java
+++ src/main/java/com/example/dncompany/controller/dn/DnController.java
@@ -1,4 +1,4 @@
-package com.example.dncompany.controller.qna;
+package com.example.dncompany.controller.dn;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -6,16 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/qna")
+@RequestMapping("/dn")
 @RequiredArgsConstructor
-public class QnaController {
+public class DnController {
 
     @GetMapping("/list")
-    public String list() {return "qna/list";}
+    public String list() {return "dn/list";}
 
     @GetMapping("/detail")
-    public String detail() {return "qna/detail";}
+    public String detail() {return "dn/detail";}
 
     @GetMapping("/write")
-    public String write() {return "qna/write";}
+    public String write() {return "dn/write";}
+
 }
