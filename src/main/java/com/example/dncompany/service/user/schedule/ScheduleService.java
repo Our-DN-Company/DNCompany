16430aa045df2b0471553002353a2370f0125ac5
JiHwanOh-prog <dhwlghks159@naver.com>
2025-01-09 14:51:54 +0900

    스케쥴 데이터 일정에 반영 ì기능 완료

--- src/main/java/com/example/dncompany/service/user/schedule/ScheduleService.java
+++ src/main/java/com/example/dncompany/service/user/schedule/ScheduleService.java
@@ -4,12 +4,14 @@ import com.example.dncompany.dto.user.schedule.ScheduleListDTO;
 import com.example.dncompany.mapper.user.schedule.ScheduleMapper;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -17,6 +19,7 @@ public class ScheduleService {
     private final ScheduleMapper scheduleMapper;
 
     public List<ScheduleListDTO> getScheduleList(Long userId) {
+
 
         return scheduleMapper.selectScheduleByUserId(userId);
     }
