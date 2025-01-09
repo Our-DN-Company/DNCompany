682c05e0bd2a2a577096d02af2adebc8b75e2d90
JiHwanOh-prog <dhwlghks159@naver.com>
2025-01-09 13:12:24 +0900

    js 수정

--- src/main/resources/static/js/user/schedule/schedule.js
+++ src/main/resources/static/js/user/schedule/schedule.js
@@ -56,7 +56,6 @@ document.addEventListener("DOMContentLoaded", function () {
     },
 
     events: [
-
       // 일정 데이터 추가 , DB의 event를 가져오려면 JSON 형식으로 변환해 events에 넣어주면된다.
       // 반복 돌려야함
         // TODO : Json 형식으로 해당 날짜의 일정을 출력해줘야함
@@ -87,11 +86,3 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   calendar.render();
 });
-
-
-
-
-
-
-
-
