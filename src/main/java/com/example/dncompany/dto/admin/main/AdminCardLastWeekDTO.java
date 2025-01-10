4314c55244aa42eac74a1b7f0cc4b2b2ec9f9ee9
jh-199802 <aodwpgus9802@gmail.com>
2025-01-10 13:24:49 +0900

    메인페이지 보드 등 기타 수정

---
+++ src/main/java/com/example/dncompany/dto/admin/main/AdminCardLastWeekDTO.java
@@ -0,0 +1,17 @@
+package com.example.dncompany.dto.admin.main;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+public class AdminCardLastWeekDTO {
+
+    private double lastWeekTotalMembers ;
+    private double lastWeekTotalBoards;
+    private double lastWeekTotalAnswers ;
+
+
+}
+
+
