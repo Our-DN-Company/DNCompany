2410c796216f059b3de76c63784e0ef75b393ab7
he11omelon <whxorud96@gmail.com>
2025-01-13 14:50:28 +0900

    qna list page 수정중

---
+++ src/main/java/com/example/dncompany/dto/qna/qnaPage/QnaBoardSearchDTO.java
@@ -0,0 +1,11 @@
+package com.example.dncompany.dto.qna.qnaPage;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+public class QnaBoardSearchDTO {
+    private String searchType;
+    private String keyword;
+}
