0259701a39b6bb1d8af1647feea2c7d876950032
JiHwanOh-prog <dhwlghks159@naver.com>
2025-01-13 14:52:31 +0900

    ë수댕냠바다 파일 업로드 작업 중

--- src/main/java/com/example/dncompany/mapper/dn/DnFileMapper.java
+++ src/main/java/com/example/dncompany/mapper/dn/DnFileMapper.java
@@ -3,9 +3,13 @@ package com.example.dncompany.mapper.dn;
 import com.example.dncompany.dto.dn.file.DnFileDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.Optional;
+
 @Mapper
 public interface DnFileMapper {
 
     void insertFile(DnFileDTO dnFileDTO);
+
+    Optional<DnFileDTO> selectByBoardId(Long freeBoardId);
 
 }
