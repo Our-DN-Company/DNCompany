a6200902b506ad6ded5d1c9af257ba5480cb3b60
DESKTOP-I7TBM5N\USER <mrldusaod@naver.com>
2025-01-14 22:59:59 +0900

    로그인 조건에 정지(활동정지 기간 추가)/ 로그인 컨트롤러 서비스 수정 및 최적화 주석 자세히

--- src/main/java/com/example/dncompany/dto/user/UserSessionDTO.java
+++ src/main/java/com/example/dncompany/dto/user/UserSessionDTO.java
@@ -4,9 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Date;
+
 @Getter @Setter @ToString
 public class UserSessionDTO {
     private Long usersId;
     private String loginId;
     private String role;
+
+    private String userStatus;     // 추가
+    private Date banStartDate;     // 추가
+    private Date banEndDate;       // 추가
 }
