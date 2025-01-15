678687febe898e0907d8ccbd6631b37cf74287ab
jh-199802 <aodwpgus9802@gmail.com>
2025-01-15 09:39:59 +0900

    Mapper.xml 안쓰는 코드 삭제/ todo리스트로 곧 삭제할 코드 명시

--- src/main/java/com/example/dncompany/service/admin/AdminBoardService.java
+++ src/main/java/com/example/dncompany/service/admin/AdminBoardService.java
@@ -48,24 +48,24 @@ public class AdminBoardService {
 
     // @TODO 카테고리별 게시물 조회 (쿼리 완성시 case 하나씩 추가 예정)
     // 옛날 코드 곧 제거 예정
-    public List<?> getBoardByCategory(String category) {
-        // 카테고리에 따라 적절한 매퍼 메소드 호출
-        switch (category.toLowerCase()) {
-            case "qna":
-                return adminBoardMapper.selectQnABoard();
-            case "신고":
-                return adminBoardMapper.selectReportBoard();
-            case "도와주세요":
-                return adminBoardMapper.selectHelpBoard();
-            case "이벤트":
-                return adminBoardMapper.selectEventBoard();
-            case "all":
-                // 전체 목록 조회 로직 (구현 필요)
-                return adminBoardMapper.selectAllBoard();
-            default:
-                return adminBoardMapper.selectReportBoard();
-        }
-    }
+//    public List<?> getBoardByCategory(String category) {
+//        // 카테고리에 따라 적절한 매퍼 메소드 호출
+//        switch (category.toLowerCase()) {
+//            case "qna":
+//                return adminBoardMapper.selectQnABoard();
+//            case "신고":
+//                return adminBoardMapper.selectReportBoard();
+//            case "도와주세요":
+//                return adminBoardMapper.selectHelpBoard();
+//            case "이벤트":
+//                return adminBoardMapper.selectEventBoard();
+//            case "all":
+//                // 전체 목록 조회 로직 (구현 필요)
+//                return adminBoardMapper.selectAllBoard();
+//            default:
+//                return adminBoardMapper.selectReportBoard();
+//        }
+//    }
 
     // 검색 조건에 따른 게시물 조회
     public List<?> getBoardBySearchCondition(BoardSearchDTO searchDTO) {
