6f11309c5987d81cc6b57037f3d5e92cbe5ea7ff
YourName <your.email@example.com>
2025-01-17 12:18:00 +0900

    ì신청 중복 처리

--- src/main/java/com/example/dncompany/dto/help/HelpOfferListDTO.java
+++ src/main/java/com/example/dncompany/dto/help/HelpOfferListDTO.java
@@ -4,13 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.time.LocalDateTime;
-
 @Getter@Setter@ToString
 public class HelpOfferListDTO {
     private Long helpOfferId;
     private String helpOfferStatus;
-    private LocalDateTime helpOfferEnrollDate;
     private Long helpId;
     private Long usersId;
 }
