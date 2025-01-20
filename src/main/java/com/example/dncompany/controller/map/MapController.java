5d203a8c58a3d4dc9c73a8eb5a141e1047cd6abe
JiHwanOh-prog <dhwlghks159@naver.com>
2025-01-20 23:59:28 +0900

    ëapi키 숨김 처리 진행ã중

--- src/main/java/com/example/dncompany/controller/map/MapController.java
+++ src/main/java/com/example/dncompany/controller/map/MapController.java
@@ -1,7 +1,9 @@
 package com.example.dncompany.controller.map;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,12 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/map")
 @RequiredArgsConstructor
 public class MapController {
+    @Value("${map.kakao-map.service.key}")
+    private String kakaoMapKey;
+
     @GetMapping("/hospital")
     public String hospital() {
         return "map/hospital";
     }
     @GetMapping("/pet-service")
-    public String petService() {
+    public String petService(Model model) {
+        model.addAttribute("kakaoMapKey", kakaoMapKey);
         return "map/pet-service";
     }
 }
