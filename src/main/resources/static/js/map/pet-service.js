890c5546ba9c5cc13ba80da937771ee630a8f166
JiHwanOh-prog <dhwlghks159@naver.com>
2025-01-20 14:06:36 +0900

    perSetvice 수정

--- src/main/resources/static/js/map/pet-service.js
+++ src/main/resources/static/js/map/pet-service.js
@@ -37,7 +37,6 @@ relayout();
 
 // 3. 페이지 처리 함수 불러오기
 // pagination 데이터 변수 정의
-
 var pagination = {
     current: 1, // 현재 페이지 번호
     perPage: 10, // 한 페이지당 가져올 데이터 개수
@@ -97,63 +96,41 @@ function displayPagination(pagination) {
     paginationEl.appendChild(fragment);
 }
 
-// 4. callback으로 받은 Json 데이터를 mapApi에 저장
-petServiceMapApi(function (mapApi) {
-    // 초기 로드 시 모든 장소의 마커를 표시
-    displayPlaces(mapApi.data);
+// 검색시 기존 마커는 제거하는 로직
+function clearMarkers() {
+    markers.forEach(function(marker) {
+        marker.setMap(null);
+    });
+    markers = [];
+}
 
-    pagination.current = 1; // 현재 페이지 초기화
-    fetchPlaces(pagination.current, pagination.perPage);
+function displayPlaces(places) {
+    // 검색 전 기존 마커 지우는 로직
+    clearMarkers();
 
-    function searchPlaces(keyword) {
-        if (!keyword || keyword.trim() === "") {
-            alert("검색어를 입력해주세요.");
-            return;
-        }
-        keyword = keyword.trim();
+    // 필터링된 장소 확인 (디버깅용)
+    console.log("Displaying Places:", places);
 
-        // mapApi.data에서 키워드로 필터링
-        var filteredData = mapApi.data.filter(function (place) {
-            return place.store_name.includes(keyword)|| // 키워드가 store_name에 포함된 항목만 반환
-                place.address_road_name.includes(keyword);  // 키워드가 address_road_name에 포함된 항목만 반환
+    // JSON 데이터를 기반으로 마커를 생성하고 지도에 표시
+    const markerMap = new Map();
+    places.forEach(function (location) {
+        const markerPosition = new kakao.maps.LatLng(location.lat, location.lng);
+
+        const marker = new kakao.maps.Marker({
+            position: markerPosition,
+            map: map,
+            title: location.store_name
         });
 
-        // 검색된 장소를 지도에 표시
-        displayPlaces(filteredData);
-    }
+        // Map 객체에 장소와 마커를 매핑
+        markerMap.set(location.store_name, marker);
 
-    // 검색시 기존 마커는 제거하는 로직
-    function clearMarkers() {
-        markers.forEach(function(marker) {
-            marker.setMap(null);
-        });
-        markers = [];
-    }
+        // 마커에 클릭 이벤트를 등록하여 인포윈도우를 표시
+        kakao.maps.event.addListener(marker, 'click', function () {
+            // 기존 인포윈도우가 열려 있으면 닫기
+            if (currentInfowindow) currentInfowindow.close();
 
-    function displayPlaces(places) {
-        // 검색 전 기존 마커 지우는 로직
-        clearMarkers();
-        // JSON 데이터를 기반으로 마커를 생성하고 지도에 표시
-        const markerMap = new Map();
-        places.forEach(function(location) {
-            const markerPosition = new kakao.maps.LatLng(location.lat, location.lng);
-            const markerKey = `${location.lat},${location.lng}`; // 고유 키 생성 (lat,lng 조합)
-
-            const marker = new kakao.maps.Marker({
-                position: markerPosition,
-                map: map,
-                title: location.store_name
-            });
-
-            // Map 객체에 장소 위치와 마커를 매핑
-            markerMap.set(markerKey, marker);
-
-            // 마커에 클릭 이벤트를 등록하여 인포윈도우를 표시
-            kakao.maps.event.addListener(marker, 'click', function() {
-                // 기존 인포윈도우가 열려 있으면 닫기
-                if (currentInfowindow) currentInfowindow.close();
-
-                const content = `
+            const content = `
                         <div style="padding:5px;">
                             <strong>${location.store_name}</strong><br>
                             주소: ${location.address_road_name}<br>
@@ -161,55 +138,90 @@ petServiceMapApi(function (mapApi) {
                         </div>
                     `;
 
-                const infowindow = new kakao.maps.InfoWindow({
-                    content: content,
-                    removable: true
-                });
-                infowindow.open(map, marker);
-                currentInfowindow = infowindow;  // 열린 인포윈도우 추적
-                // 마커 클릭 시 해당 마커의 위치로 지도 중심 이동
-                map.setCenter(markerPosition);
+            const infowindow = new kakao.maps.InfoWindow({
+                content: content,
+                removable: true
             });
-
-            // 생성된 마커를 배열에 추가
-            markers.push(marker);
+            infowindow.open(map, marker);
+            currentInfowindow = infowindow;  // 열린 인포윈도우 추적
+            // 마커 클릭 시 해당 마커의 위치로 지도 중심 이동
+            map.setCenter(markerPosition);
         });
 
-        // 장소 리스트 업데이트 함수 호출
-        updatePlacesList(places, markerMap);
+        // 생성된 마커를 배열에 추가
+        markers.push(marker);
+    });
+    // 장소 리스트 업데이트 함수 호출
+    updatePlacesList(places, markerMap);
+}
 
-        // searchPlaces를 전역으로 노출
-        window.searchPlaces = searchPlaces;
+function updatePlacesList(places, markerMap) {
+    const placesList = document.getElementById('placesList');
+    placesList.innerHTML = ''; // 기존 리스트 초기화
 
-        function updatePlacesList(places, markerMap) {
-            const placesList = document.getElementById('placesList');
-            placesList.innerHTML = ''; // 기존 리스트 초기화
-
-            places.forEach(function(place) {
-                const listItem = document.createElement('li');
-                const markerKey = `${place.lat},${place.lng}`; // 고유 키 생성
-
-                listItem.textContent = place.store_name;
-                listItem.className = 'item';
-                listItem.innerHTML = `
+    // , index
+    places.forEach(function(place) {
+        const listItem = document.createElement('li');
+        listItem.textContent = place.store_name;
+        listItem.className = 'item';
+        listItem.innerHTML = `
                     <div class="info">
                         <h5>${place.store_name}</h5>
                         <span>도로명 주소 : ${place.address_road_name}</span>
                         <span>지번 : ${place.address_jibun_name}</span>
-                        <span>전화번호 : ${place.phone_number || '전화번호 없음'}</span>
+                        <span>${place.phone_number || '전화번호 없음'}</span>
                     </div>
                 `;
-                // 리스트 항목 클릭 이벤트 추가
-                listItem.addEventListener("click", function () {
-                    const marker = markerMap.get(markerKey);
-                    if (marker) {
-                        kakao.maps.event.trigger(marker, "click"); // 마커의 클릭 이벤트를 트리거
-                    }
-                });
-                placesList.appendChild(listItem);
-            });
+        // 리스트 항목 클릭 이벤트 추가
+        listItem.addEventListener("click", function () {
+            const marker = markerMap.get(place.store_name);
+            if (marker) {
+                kakao.maps.event.trigger(marker, "click"); // 마커의 클릭 이벤트를 트리거
+            }
+        });
+        placesList.appendChild(listItem);
+    });
+}
+
+// 4. callback으로 받은 Json 데이터를 mapApi에 저장
+petServiceMapApi(function (mapApi) {
+    // 초기 로드 시 모든 장소의 마커를 표시
+    displayPlaces(mapApi.data);
+    pagination.current = 1; // 현재 페이지 초기화
+    fetchPlaces(pagination.current, pagination.perPage);
+
+    function
+    searchPlaces(keyword) {
+        if (!keyword || keyword.trim() === "") {
+            alert("검색어를 입력해주세요.");
+            return;
+        }
+        keyword = keyword.trim();
+
+        if (!mapApi || !Array.isArray(mapApi.data)) {
+            console.error("Invalid mapApi data");
+            return;
+        }
+
+        // mapApi.data에서 키워드로 필터링
+        var filteredData = mapApi.data.filter(function (place) {
+            return place && // place가 null 또는 undefined가 아닌지 확인
+                (place.store_name.includes(keyword)|| // 키워드가 store_name에 포함된 항목만 반환
+                place.address_road_name.includes(keyword));  // 키워드가 address_road_name에 포함된 항목만 반환
+        });
+
+        // 필터링된 데이터 확인 (디버깅용)
+        console.log("Filtered Data:", filteredData);
+
+        if (filteredData.length === 0) {
+            alert("검색 결과가 없습니다.");
+        } else {
+            // 검색된 장소를 지도에 표시
+            displayPlaces(filteredData);
         }
     }
+    // searchPlaces를 전역으로 노출
+    window.searchPlaces = searchPlaces;
 });
 
 
